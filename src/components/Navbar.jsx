4908c14f5abd01e10df9ca99a0e97423854e2d74
siseldyke <siseldyke@gmail.com>
2024-11-07 09:21:15 -0500

    navbar skeleton set up

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+const NavBar = () => {
+    return (
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/Starships'>Starships</Link>
+          </li>
+          <li>
+            <Link to='/planets'>Planets</Link>
+          </li>
+          <li>
+            <Link to='/characters'>Characters</Link>
+          </li>
+          <li>
+            <Link to='/films'>Films</Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  };
+  
+  export default NavBar;
